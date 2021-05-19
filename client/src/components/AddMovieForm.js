9b061a52b1437e7bf85d865fee10fac8fdaf38b9
Hugo-Orozco <aceasin@outlook.com>
2021-05-19 16:44:25 -0600

    MVP & Stretch Goals

--- client/src/components/AddMovieForm.js
+++ client/src/components/AddMovieForm.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 import axios from 'axios';
 
-const EditMovieForm = (props) => {
+const AddMovieForm = (props) => {
 	const { push } = useHistory();
 
 	const { setMovies } = props;
@@ -17,18 +17,6 @@ const EditMovieForm = (props) => {
 		description: ""
 	});
 
-	const { id } = useParams();
-
-	useEffect(() => {
-		axios.get(`http://localhost:5000/api/movies/${id}`)
-			.then(res => {
-				setMovie(res.data);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	}, []);
-
 	const handleChange = (e) => {
         setMovie({
             ...movie,
@@ -38,10 +26,10 @@ const EditMovieForm = (props) => {
 
     const handleSubmit = (e) => {
 		e.preventDefault();
-		axios.put(`http://localhost:5000/api/movies/${id}`, movie)
+		axios.post(`http://localhost:5000/api/movies`, movie)
 			.then(res => {
 				setMovies(res.data);
-				push(`/movies/${id}`);
+				push('/movies');
 			})
 			.catch(err => {
 				console.log(err);
@@ -55,7 +43,7 @@ const EditMovieForm = (props) => {
 		<div className="modal-content">
 			<form onSubmit={handleSubmit}>
 				<div className="modal-header">
-					<h4 className="modal-title">Editing <strong>{movie.title}</strong></h4>
+					<h4 className="modal-title">Adding <strong>{movie.title}</strong></h4>
 				</div>
 				<div className="modal-body">
 					<div className="form-group">
@@ -89,4 +77,4 @@ const EditMovieForm = (props) => {
 	</div>);
 }
 
-export default EditMovieForm;
+export default AddMovieForm;
--- client/src/components/DeleteMovieModal.js
+++ client/src/components/DeleteMovieModal.js
@@ -1,21 +1,34 @@
 import React from 'react';
 
-const DeleteMovieModal = () => {
+const DeleteMovieModal = (props) => {
+
+    const { setModal, deleteMovie, push, id } = props;
+
+    const handleCancel = () => {
+        setModal(false);
+    };
+
+    const handleDelete = () => {
+        deleteMovie(id);
+        push('/movies');
+        setModal(false);
+    };
+
     return (<div id="deleteMovieModal">
         <div className="modal-dialog">
             <div className="modal-content">
                 <form>
-                    <div className="modal-header">						
+                    <div className="modal-header">
                         <h4 className="modal-title">Delete Movie</h4>
                         <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                     </div>
-                    <div className="modal-body">					
+                    <div className="modal-body">
                         <p>Are you sure you want to delete these records?</p>
                         <p className="text-warning"><small>This action cannot be undone.</small></p>
                     </div>
                     <div className="modal-footer">
-                        <input type="button" className="btn btn-default" data-dismiss="modal" value="Cancel"/>
-                        <input type="submit" className="btn btn-danger" value="Delete"/>
+                        <input type="button" className="btn btn-default" data-dismiss="modal" value="Cancel" onClick={handleCancel}/>
+                        <input type="submit" className="btn btn-danger" value="Delete" onClick={handleDelete}/>
                     </div>
                 </form>
             </div>
