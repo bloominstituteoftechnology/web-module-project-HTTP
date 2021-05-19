596f2393e4a9c25ad9c6c1aa85c96a69414b6560
drslaughterrex <drslaughterrex@gmail.com>
2021-05-19 16:28:51 -0500

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,72 @@
 import React, { useEffect, useState } from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
-import MovieHeader from './components/MovieHeader';
+import MovieHeader from "./components/MovieHeader";
 
-import EditMovieForm from './components/EditMovieForm';
-import FavoriteMovieList from './components/FavoriteMovieList';
+import EditMovieForm from "./components/EditMovieForm";
+import FavoriteMovieList from "./components/FavoriteMovieList";
 
-import axios from 'axios';
+import axios from "axios";
 
 const App = (props) => {
-  const [movies, setMovies] = useState([]);
-  const [favoriteMovies, setFavoriteMovies] = useState([]);
+	const [movies, setMovies] = useState([]);
+	const [favoriteMovies, setFavoriteMovies] = useState([]);
 
-  useEffect(()=>{
-    axios.get('http://localhost:5000/api/movies')
-      .then(res => {
-        setMovies(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, []);
+	useEffect(() => {
+		axios
+			.get("http://localhost:5000/api/movies")
+			.then((res) => {
+				setMovies(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-  const deleteMovie = (id)=> {
-  }
+	const deleteMovie = (id) => {
+		setMovies(movies.filter((item) => item.id !== Number(id)));
+	};
 
-  const addToFavorites = (movie) => {
-    
-  }
+	const addToFavorites = (movie) => {};
 
-  return (
-    <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/> HTTP / CRUD Module Project</span>
-      </nav>
+	return (
+		<div>
+			<nav className="navbar navbar-dark bg-dark">
+				<span className="navbar-brand">
+					<img width="40px" alt="" src="./Lambda-Logo-Red.png" /> HTTP / CRUD
+					Module Project
+				</span>
+			</nav>
 
-      <div className="container">
-        <MovieHeader/>
-        <div className="row ">
-          <FavoriteMovieList favoriteMovies={favoriteMovies}/>
-        
-          <Switch>
-            <Route path="/movies/edit/:id">
-            </Route>
+			<div className="container">
+				<MovieHeader />
+				<div className="row ">
+					<FavoriteMovieList favoriteMovies={favoriteMovies} />
 
-            <Route path="/movies/:id">
-              <Movie/>
-            </Route>
+					<Switch>
+						<Route path="/movies/edit/:id">
+							<EditMovieForm setMovies={setMovies} />
+						</Route>
 
-            <Route path="/movies">
-              <MovieList movies={movies}/>
-            </Route>
+						<Route path="/movies/:id">
+							<Movie deleteMovie={deleteMovie} />
+						</Route>
 
-            <Route path="/">
-              <Redirect to="/movies"/>
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </div>
-  );
+						<Route path="/movies">
+							<MovieList movies={movies} />
+						</Route>
+
+						<Route path="/">
+							<Redirect to="/movies" />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</div>
+	);
 };
 
-
 export default App;
-
