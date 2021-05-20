5b4c9df2f8cd584c802cf92314568217012a1cd8
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-05-19 20:27:00 -0400

    Project complete

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -4,7 +4,7 @@ import { Link, useParams, useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 const Movie = (props) => {
-    const { addToFavorites } = props;
+    const { addToFavorites, deleteMovie } = props;
 
     const [movie, setMovie] = useState('');
 
@@ -17,7 +17,7 @@ const Movie = (props) => {
                 setMovie(res.data);
             })
             .catch(err=>{
-                console.log(err.response);
+                console.log(err);
             })
     }, [id]);
 
@@ -52,7 +52,7 @@ const Movie = (props) => {
                         <section>
                             <span className="m-2 btn btn-dark">Favorite</span>
                             <Link to={`/movies/edit/${movie.id}`} className="m-2 btn btn-success">Edit</Link>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete" onClick={() => deleteMovie(id)}/></span>
                         </section>
                     </div>
                 </div>
