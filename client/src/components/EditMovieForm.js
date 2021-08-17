25189f31b229387fad107745042c77cd441fcb0a
Rick <rmansfield@realpeoplesratings.com>
2021-08-17 17:39:05 -0500

    Completed #5 see readme.md

--- client/src/components/EditMovieForm.js
+++ client/src/components/EditMovieForm.js
@@ -6,11 +6,15 @@ import axios from 'axios';
 
 const EditMovieForm = (props) => {
 	const { push } = useHistory();
-		//2 Next, we need to grab the id being passed into the component through the url. Use the `useParams` hook to get the id value.
-		const { id } = useParams();
+	//2 Next, we need to grab the id being passed into the component through the url. Use the `useParams` hook to get the id value.
+	const { id } = useParams();
+
+	//5 Don't forget to make sure that your server data and your local state are in sync! Make any changes the edit route needed to give the edit form access to App's `setMovies` method.
+	const { setMovies } = props; // look at the handleSubmit carefully and make sure the setMovies is plural too. 
+
 
 	const [movie, setMovie] = useState({
-		title:"",
+		title: "",
 		director: "",
 		genre: "",
 		metascore: 0,
@@ -18,22 +22,22 @@ const EditMovieForm = (props) => {
 	});
 
 	//3. We need to be able to load in the current movie's attributes into our local form state. When `EditMovieForm` mount, retrieve our current id's movie from the api and save the data returned to local state. Hence a useEffect and GET req, like on didMount here and add the ${id}
-		useEffect(() => {
-			axios.get(`http://localhost:5000/api/movies/${id}`)
-				.then(res => {
-					setMovie(res.data);
-				})
-				.catch(err => {
-					console.log(err.response);
-				})
-		}, [id]);
-	
+	useEffect(() => {
+		axios.get(`http://localhost:5000/api/movies/${id}`)
+			.then(res => {
+				setMovie(res.data);
+			})
+			.catch(err => {
+				console.log(err.response);
+			})
+	}, [id]);
+
 	const handleChange = (e) => {
-        setMovie({
-            ...movie,
-            [e.target.name]: e.target.value
-        });
-    }
+		setMovie({
+			...movie,
+			[e.target.name]: e.target.value
+		});
+	}
 
 	//4. At this point, nothing happens when the edit form is submitted. Add in the api call needed to update the server with our updated movie data.
 	const handleSubmit = (e) => {
@@ -50,43 +54,43 @@ const EditMovieForm = (props) => {
 
 	const { title, director, genre, metascore, description } = movie;
 
-    return (
-	<div className="col">
-		<div className="modal-content">
-			<form onSubmit={handleSubmit}>
-				<div className="modal-header">						
-					<h4 className="modal-title">Editing <strong>{movie.title}</strong></h4>
-				</div>
-				<div className="modal-body">					
-					<div className="form-group">
-						<label>Title</label>
-						<input value={title} onChange={handleChange} name="title" type="text" className="form-control"/>
+	return (
+		<div className="col">
+			<div className="modal-content">
+				<form onSubmit={handleSubmit}>
+					<div className="modal-header">
+						<h4 className="modal-title">Editing <strong>{movie.title}</strong></h4>
 					</div>
-					<div className="form-group">
-						<label>Director</label>
-						<input value={director} onChange={handleChange} name="director" type="text" className="form-control"/>
+					<div className="modal-body">
+						<div className="form-group">
+							<label>Title</label>
+							<input value={title} onChange={handleChange} name="title" type="text" className="form-control" />
+						</div>
+						<div className="form-group">
+							<label>Director</label>
+							<input value={director} onChange={handleChange} name="director" type="text" className="form-control" />
+						</div>
+						<div className="form-group">
+							<label>Genre</label>
+							<input value={genre} onChange={handleChange} name="genre" type="text" className="form-control" />
+						</div>
+						<div className="form-group">
+							<label>Metascore</label>
+							<input value={metascore} onChange={handleChange} name="metascore" type="number" className="form-control" />
+						</div>
+						<div className="form-group">
+							<label>Description</label>
+							<textarea value={description} onChange={handleChange} name="description" className="form-control"></textarea>
+						</div>
+
 					</div>
-					<div className="form-group">
-						<label>Genre</label>
-						<input value={genre} onChange={handleChange} name="genre" type="text" className="form-control"/>
+					<div className="modal-footer">
+						<input type="submit" className="btn btn-info" value="Save" />
+						<Link to={`/movies/1`}><input type="button" className="btn btn-default" value="Cancel" /></Link>
 					</div>
-					<div className="form-group">
-						<label>Metascore</label>
-						<input value={metascore} onChange={handleChange} name="metascore" type="number" className="form-control"/>
-					</div>		
-					<div className="form-group">
-						<label>Description</label>
-						<textarea value={description} onChange={handleChange} name="description" className="form-control"></textarea>
-					</div>
-									
-				</div>
-				<div className="modal-footer">			    
-					<input type="submit" className="btn btn-info" value="Save"/>
-					<Link to={`/movies/1`}><input type="button" className="btn btn-default" value="Cancel"/></Link>
-				</div>
-			</form>
-		</div>
-	</div>);
+				</form>
+			</div>
+		</div>);
 }
 
 export default EditMovieForm;
