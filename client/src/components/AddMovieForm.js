661495e78a81677b19d18f52b319154ceda96be1
Klove Adams <kalebsxott@gmail.com>
2021-08-18 17:45:40 -0400

    Started making AddMovieForm

---
+++ client/src/components/AddMovieForm.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory, Link } from 'react-router-dom';
+import axios from 'axios';
+
+const AddMovieForm = (props) => {
+    const { push } = useHistory();
+	const { id } = useParams()
+}
+
+export default AddMovieForm;
