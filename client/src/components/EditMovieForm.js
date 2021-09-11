0f79b692542ff9472cfd330a50379047aa853da9
tamara-taylor <ttaylor8522@gmail.com>
2021-09-11 11:26:03 -0400

    finished mvp

--- client/src/components/EditMovieForm.js
+++ client/src/components/EditMovieForm.js
@@ -6,6 +6,8 @@ import axios from 'axios';
 
 const EditMovieForm = (props) => {
 	const { push } = useHistory();
+	const {id} = useParams()
+	const {setMovies} = props
 
 	const [movie, setMovie] = useState({
 		title:"",
@@ -14,6 +16,16 @@ const EditMovieForm = (props) => {
 		metascore: 0,
 		description: ""
 	});
+
+	useEffect(() => {
+		axios.get('http://localhost:5000/api/movies/${id}')
+		.then(res => {
+			setMovie(id)
+		})
+		.catch(error => {
+			console.log(error)
+		})
+	}, [id])
 	
 	const handleChange = (e) => {
         setMovie({
@@ -24,6 +36,11 @@ const EditMovieForm = (props) => {
 
     const handleSubmit = (e) => {
 		e.preventDefault();
+		axios.put('http://localhost:5000/api/movies/${id}', movie)
+		.then(res => {
+			setMovies(res.data)
+			push('/movies/${id}')
+		})
 	}
 	
 	const { title, director, genre, metascore, description } = movie;
--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -4,7 +4,7 @@ import { Link, useParams, useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 const Movie = (props) => {
-    const { addToFavorites } = props;
+    const { deleteMovie } = props;
 
     const [movie, setMovie] = useState('');
 
@@ -20,6 +20,14 @@ const Movie = (props) => {
                 console.log(err.response);
             })
     }, [id]);
+
+    const handleDeleteClick = () => {
+        axios.delete('http://localhost:5000/api/movies/${id}')
+        .then(res => {
+            deleteMovie(res.data)
+            push('/movies')
+        })
+    }
 
     return(<div className="modal-page col">
         <div className="modal-dialog">
@@ -52,7 +60,7 @@ const Movie = (props) => {
                         <section>
                             <span className="m-2 btn btn-dark">Favorite</span>
                             <Link to={`/movies/edit/${movie.id}`} className="m-2 btn btn-success">Edit</Link>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete" onClick='handleDeleteClick' /></span>
                         </section>
                     </div>
                 </div>
