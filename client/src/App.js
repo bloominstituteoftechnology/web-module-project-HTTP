414cb0fc9bbe4d59ee49c3f74a1970179693b7ff
Richard Oh <n24512@protonmail.com>
2021-09-15 15:27:52 -0500

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Movie from "./components/Movie";
 
 import MovieHeader from "./components/MovieHeader";
 
+import AddMovieForm from "./components/AddMovieForm";
 import EditMovieForm from "./components/EditMovieForm";
 import FavoriteMovieList from "./components/FavoriteMovieList";
 
@@ -49,6 +50,10 @@ const App = (props) => {
           <Switch>
             <Route path="/movies/edit/:id">
               <EditMovieForm setMovies={setMovies} />
+            </Route>
+
+            <Route path="/movies/add">
+              <AddMovieForm setMovies={setMovies} />
             </Route>
 
             <Route path="/movies/:id">
