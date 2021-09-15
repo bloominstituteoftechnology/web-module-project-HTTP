c12ff9e61b97a274f20fcd9a5d7e6ea9d23813a5
Richard Oh <n24512@protonmail.com>
2021-09-15 15:07:42 -0500

    delete movie complete, still shows up without refresh

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,9 @@ const App = (props) => {
       });
   }, []);
 
-  const deleteMovie = (id) => {};
+  const deleteMovie = (id) => {
+    setMovies(movies.filter((item) => item.id !== id));
+  };
 
   const addToFavorites = (movie) => {};
 
@@ -50,7 +52,7 @@ const App = (props) => {
             </Route>
 
             <Route path="/movies/:id">
-              <Movie />
+              <Movie deleteMovie={deleteMovie} />
             </Route>
 
             <Route path="/movies">
