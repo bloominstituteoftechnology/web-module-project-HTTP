97bf03b6803b76bf11480effb372167ccae3505f
Angelique Abacajan <ma_utsey@yahoo.com>
2021-11-10 17:15:47 -0500

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import MovieHeader from './components/MovieHeader';
 
 import EditMovieForm from './components/EditMovieForm';
 import FavoriteMovieList from './components/FavoriteMovieList';
+import AddMovieForm from "./components/AddMovieForm";
 
 import axios from 'axios';
 
