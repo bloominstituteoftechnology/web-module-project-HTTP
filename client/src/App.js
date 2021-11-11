880aca910ae8b14714195f2bd8e744d62fe5db18
Leo Oladimu <leooladimu@Leos-MacBook-Air.local>
2021-11-10 22:00:12 -0500

    1

--- client/src/App.js
+++ client/src/App.js
@@ -10,22 +10,27 @@ import EditMovieForm from './components/EditMovieForm';
 import FavoriteMovieList from './components/FavoriteMovieList';
 
 import axios from 'axios';
+import AddMovie from "./components/AddMovie";
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
 
-  useEffect(()=>{
-    axios.get('http://localhost:5000/api/movies')
-      .then(res => {
-        setMovies(res.data);
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/movies')
+      .then(r => {
+        setMovies(r.data);
       })
       .catch(err => {
         console.log(err);
       });
   }, []);
 
-  const deleteMovie = (id)=> {
+  const deleteMovie = (id) => {
+    setMovies(
+      movies.filter(
+        movie => movie.id !== Number(id)
+    ));
   }
 
   const addToFavorites = (movie) => {
@@ -35,7 +40,7 @@ const App = (props) => {
   return (
     <div>
       <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/> HTTP / CRUD Module Project</span>
+        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/> HTTP / CRUD Module Project ∞</span>
       </nav>
 
       <div className="container">
@@ -45,6 +50,7 @@ const App = (props) => {
         
           <Switch>
             <Route path="/movies/edit/:id">
+              <EditMovieForm setMovies = { setMovies } /> 
             </Route>
 
             <Route path="/movies/:id">
@@ -67,4 +73,3 @@ const App = (props) => {
 
 
 export default App;
-
