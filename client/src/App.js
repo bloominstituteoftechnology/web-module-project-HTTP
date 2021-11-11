88273aeae46ccb6a34def5da48081e18c1cd4d12
FrameofLightDesigner <31264591+FrameofLightDesigner@users.noreply.github.com>
2021-11-10 19:49:30 -0500

    first and final push

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Movie from './components/Movie';
 
 import MovieHeader from './components/MovieHeader';
 
+import AddMovieForm from './components/AddMovieForm';
 import EditMovieForm from './components/EditMovieForm';
 import FavoriteMovieList from './components/FavoriteMovieList';
 
@@ -14,57 +15,62 @@ import axios from 'axios';
 const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
-
+  console.log("App: ", movies);
   useEffect(()=>{
     axios.get('http://localhost:5000/api/movies')
-      .then(res => {
-        setMovies(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+        .then(res => {
+          setMovies(res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
   }, []);
 
   const deleteMovie = (id)=> {
+    setMovies(movies.filter(movie => movie.id = Number(id)));
   }
 
   const addToFavorites = (movie) => {
-    
+
   }
 
   return (
-    <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/> HTTP / CRUD Module Project</span>
-      </nav>
+      <div>
+        <nav className="navbar navbar-dark bg-dark">
+          <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/> HTTP / CRUD Module Project</span>
+        </nav>
 
-      <div className="container">
-        <MovieHeader/>
-        <div className="row ">
-          <FavoriteMovieList favoriteMovies={favoriteMovies}/>
-        
-          <Switch>
-            <Route path="/movies/edit/:id">
-            </Route>
+        <div className="container">
+          <MovieHeader/>
+          <div className="row ">
+            <FavoriteMovieList favoriteMovies={favoriteMovies}/>
 
-            <Route path="/movies/:id">
-              <Movie/>
-            </Route>
+            <Switch>
+              <Route path="/movies/add" >
+                <AddMovieForm component={AddMovieForm} setMovies={setMovies}/>
+              </Route>
 
-            <Route path="/movies">
-              <MovieList movies={movies}/>
-            </Route>
+              <Route path="/movies/edit/:id" >
+                <EditMovieForm component={EditMovieForm} setMovies={setMovies}/>
+              </Route>
 
-            <Route path="/">
-              <Redirect to="/movies"/>
-            </Route>
-          </Switch>
+              <Route path="/movies/:id">
+                <Movie deleteMovie={deleteMovie}/>
+              </Route>
+
+              <Route path="/movies">
+                <MovieList movies={movies}/>
+              </Route>
+
+              <Route path="/">
+                <Redirect to="/movies"/>
+              </Route>
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
 
 export default App;
-
--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -4,7 +4,7 @@ import { Link, useParams, useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 const Movie = (props) => {
-    const { addToFavorites } = props;
+    const { addToFavorites, deleteMovie } = props;
 
     const [movie, setMovie] = useState('');
 
@@ -21,10 +21,23 @@ const Movie = (props) => {
             })
     }, [id]);
 
+    const handleDelete = () => {
+        console.log("delete clicked!");
+        axios.delete(`http://localhost:5000/api/movies/${id}`)
+            .then(res => {
+                // console.log(res);
+                deleteMovie(id);
+                push('/movies');
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }
+
     return(<div className="modal-page col">
         <div className="modal-dialog">
             <div className="modal-content">
-                <div className="modal-header">						
+                <div className="modal-header">
                     <h4 className="modal-title">{movie.title} Details</h4>
                 </div>
                 <div className="modal-body">
@@ -48,11 +61,11 @@ const Movie = (props) => {
                                 <p><strong>{movie.description}</strong></p>
                             </div>
                         </section>
-                        
+
                         <section>
                             <span className="m-2 btn btn-dark">Favorite</span>
                             <Link to={`/movies/edit/${movie.id}`} className="m-2 btn btn-success">Edit</Link>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete" onClick={handleDelete}/></span>
                         </section>
                     </div>
                 </div>
